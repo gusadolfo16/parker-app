5a3e189482a3e8744936ca3b8656a584a3e74af7
Gus <gusadolfo@gmail.com>
2025-11-28 13:22:30 -0300

    fix: Resolve selection click issue on home page
    - Increase z-index of SelectTileOverlay to z-50 to ensure it captures clicks above photo links
    - Update documentation

--- src/components/SelectTileOverlay.tsx
+++ src/components/SelectTileOverlay.tsx
@@ -19,6 +19,7 @@ export default function SelectTileOverlay({
   return (
     <div className={clsx(
       'absolute w-full h-full',
+      'z-50',
       !disabled && 'cursor-pointer',
       'active:bg-gray-950/40 dark:active:bg-gray-950/60',
       isPerformingSelectEdit && 'pointer-events-none',
